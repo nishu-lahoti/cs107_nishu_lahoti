40530d4be552e2cfc1c3145d11277b9154d4d1a4
nishu-lahoti <nishu.lahoti@Nishus-MacBook-Pro.local>
2020-09-14 21:18:38 -0400

    Committing updates to P3.sh and P3F.sh

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -2,5 +2,5 @@
 cat apollo13.txt | grep -c [0-9]
 grep --help | grep "\-c,"
 find . -type f | grep "\.py" | wc -l
-find . -type f -perm 001
-find . -type f,d -perm 001
+find . -type f -perm 001 | wc -l
+find . -type f,d -perm 001 | wc -l
