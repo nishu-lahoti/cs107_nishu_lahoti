1d1846b6a2c7615684260d6e04459d747d5222ba
nishu-lahoti <nishu.lahoti@Nishus-MacBook-Pro.local>
2020-10-08 18:24:29 -0400

    Added dunder methods for P3a

--- homework/HW3/HW3-final/Bank.py
+++ homework/HW3/HW3-final/Bank.py
@@ -27,13 +27,11 @@ class BankAccount():
         return "Hello {0} you have accessed your {1} account!".format(self.owner, self.accountType)
     
     def __len__(self):
-        # your code
+        return len(self.balance)
 
 a1 = BankAccount("Nishu", "CHECKING", 100000)
-# a2 = BankAccount("Anahide Nahhal", "SAVING",  50000)
 
 a1.deposit(1000)
 a1.withdraw(5000)
-a2.withdraw(10000)
-a2.withdraw(100000)
 str(a1)
+len(a1)
