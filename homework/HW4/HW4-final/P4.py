e69da234519bb3398fd99bda8086316b5e950e67
nishu-lahoti <nishu.lahoti@Nishus-MacBook-Pro.local>
2020-10-26 15:55:56 -0400

    Adding updates to P2, P3, and P4

--- homework/HW4/HW4-final/P4.py
+++ homework/HW4/HW4-final/P4.py
@@ -1,48 +1,53 @@
 class AutoDiffToy:
 
     # Initializing with value for x
-    def __init__(self, value):
-        self.value = value
-
-    def val(self):
-        return self.value
-
-    ### Compute derivative of input value ###
+    def __init__(self, value, der = 1):
+        self.val = value
+        self.der = der
 
     # Forward add with object and exception handling
     def __add__(self, other):
         try:
-            return AutoDiffToy(self.value + other.value)
+            new_val = self.val + other.val
+            der_val = self.der
         except AttributeError:
-            return AutoDiffToy(self.value + other)
-
+            new_val = self.val + other
+            der_val = self.der
+        return AutoDiffToy(new_val, der_val)
+        
     # Multiplication function
     def __mul__(self, other):
         try:
-            return AutoDiffToy(self.value * other.value)
+            new_val = self.val * other.val
+            der_val = self.der * other.val
         except AttributeError:
-            return AutoDiffToy(self.value * other)
-    
-    # Reverse add to allow for different equation patterns
+            new_val = self.val * other
+            der_val = self.der * other
+        return AutoDiffToy(new_val, der_val)
+
+    # Reverse add by calling __add__
     def __radd__(self, other):
-        try:
-            return AutoDiffToy(self.value + other.value)
-        except AttributeError:
-            return (self.value + other)
+        return self.__add__(other)
 
-    # Reverse multiply to allow for different equation patters
+
+    # Reverse multiply by calling __mul__
     def __rmul__(self, other):
-        try:
-            return AutoDiffToy(self.value * other.value)
-        except AttributeError:
-            return AutoDiffToy(self.value * other)
+        return self.__mul__(other)
 
+
+# Demo
 
 a = 2
 x = AutoDiffToy(a)
 
 alpha = 2.0
 beta = 3.0
-f = alpha * x + beta
 
-# print(f.val, f.der) <--- How can f become an object with .val and .der?
+f = alpha * x + beta
+print(f.val, f.der)
+f = x * alpha + beta
+print(f.val, f.der)
+f = beta + alpha * x
+print(f.val, f.der)
+f = beta + x * alpha
+print(f.val, f.der)
