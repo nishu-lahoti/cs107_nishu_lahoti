c2fdc2dc5581563b2c6d62e62711024499c5ba68
nishu-lahoti <nishu.lahoti@Nishus-MacBook-Pro.local>
2020-11-03 12:12:37 -0500

    Created animal.py and complete P1A

---
+++ homework/HW5/HW5-final/animal.py
@@ -0,0 +1,28 @@
+class Animal:
+
+    # a class attribute of the valid species in our universe
+    valid_species = {
+        'cat',
+        'dog',
+        'duck',
+        'elf',
+        'goblin',
+        'horse',
+        'human',
+        'mermaid',
+        'nightingale',
+        'pig',
+        'swan',
+        'wolf'
+    }
+    
+    def __init__(self, name, species):
+        self.name = name
+        self._species = species
+
+    def __repr__(self):
+        return f'{self.name} ({self._species})'
+
+    @property
+    def species(self):
+        return self._species
