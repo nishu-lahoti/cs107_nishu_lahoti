600bf2984f66aee946ec40d4fd9804d5a63a1bfc
nishu-lahoti <nishu.lahoti@Nishus-MacBook-Pro.local>
2020-11-23 21:32:28 -0500

    Cleaned up graphs for P3

--- homework/HW6/HW6-final/P3-C.py
+++ homework/HW6/HW6-final/P3-C.py
@@ -10,9 +10,9 @@ python_priority = timeit(ns=(10, 20, 50, 100, 200, 500), pqclass = PythonHeapPri
 x_axis = [10, 20, 50, 100, 200, 500]
 
 fig, ax = plt.subplots(1, 1, figsize=(10, 6), constrained_layout = True)
-ax.set_xlabel(r'$Number of Lists Merged$', fontsize = 16)
-ax.set_ylabel(r'$Elapsted time in seconds$', fontsize = 16)
-ax.set_title(r'Comparing times for different priority queues', fontsize = 16)
+ax.set_xlabel(r'Number of Lists Merged', fontsize = 16)
+ax.set_ylabel(r'Elapsed time in seconds', fontsize = 16)
+ax.set_title(r'Comparing performance for different priority queues', fontsize = 16)
 
 ax.plot(x_axis, naive_priority, label = r'Naive Priority Queue')
 ax.plot(x_axis, heap_priority, label = r'Heap Priority Queue')
