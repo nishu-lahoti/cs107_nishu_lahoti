b9c560566ac6e7abb19f80fa8b682dd6dc64c02f
nishu-lahoti <nishu.lahoti@Nishus-MacBook-Pro.local>
2020-12-01 19:38:24 -0500

    Committing updates after office hours

--- homework/HW7/HW7-final/Markov.py
+++ homework/HW7/HW7-final/Markov.py
@@ -1,23 +1,27 @@
 import numpy as np
-from enum import Enum
 
 class Markov:
 
-    def __init__(self):
+    def __init__(self, day_zero_weather = None):
         self.data = []
         self.weather_types = {"sunny": 0, "cloudy": 1, "rainy": 2, "snowy": 3, "windy": 4, "hailing": 5}
-        # self.weather_types = ["sunny", "cloudy", "rainy", "snowy", "windy", "hailing"]
+        self._current_day_weather = day_zero_weather
 
     def __iter__(self):
         # Returns a MarkovIterator object.
-        # Hint: self.get_prob() might come in handy here.
 
-        return MarkovIterator(self.weather_types, self.data)
+        weather_probability_dict = dict()
+        weather_probability = [self.get_prob(self._current_day_weather, w) for w in self.weather_types]
+        print(weather_probability)
+
+        for i in self.weather_types:
+            weather_probability_dict[i] = [self.get_prob(i, w) for w in self.weather_types]
+
+        return MarkovIterator(self.weather_types, weather_probability_dict, weather_probability, 3)
 
     def load_data(self, file_path = './weather.csv'):
         self.data = np.genfromtxt(file_path, dtype = float, delimiter = ",")
-        # print(self.data)
-    
+
     def get_prob(self, current_day_weather, next_day_weather):
         
         # Inputs must be one of the six specified weather types
@@ -35,51 +39,37 @@ class Markov:
 
 class MarkovIterator:
 
-    def __init__(self, weather_types, transition_matrix):
+    def __init__(self, weather_types, transition_matrix, weather_probability, days):
         
-        # Useful attributes to help you with the next method
         # Initialize the data values and weather types from the Markov class.
-        self.day_zero_weather = None
         self.weather_types = weather_types
-        self.transition_matrix = transition_matrix # np.atleast_2d(transition_matrix)
-        self.index_dict = {self.weather_types[index]: index for index in range(len(self.weather_types))}
+        self.transition_matrix = transition_matrix 
+        self.weather_probability = weather_probability
+        self.days = days
         
 
     def __iter__(self):
         return self
 
-    def __next__(self, current_day_weather):
+    def __next__(self):
 
         # Use np.random.choice to select one of the weather types based on the probabilities inside
         # the transition matrix. Question is: how do I inform the transition matrix to call the right column?
-        return np.random.choice(
-            self.weather_types, p = self.transition_matrix[self.index_dict[current_day_weather], :]
-        )
+        
         # The next day's weather should be randomly selected based on the relative probabilities of the next day's weather types
         # given the current day's weather type. The next day's weather should be returned as a lowercase string value (i.e. 'sunny').
+        next_day_weather = np.random.choice(self.weather_types, p = self.transition_matrix)
+        self.weather_probability = self.transition_matrix[next_day_weather]
+
 
         # Once we determine the next day's weather prediction (n+1), we should be able to store it and use it to predict the next days
         # (n+2) weather prediction. This may require switching next_day and current_day.
         # numpy.random.choice might be useful for selecting net day weather.
-
-        # Online example: https://medium.com/@__amol__/markov-chains-with-python-1109663f3678
-
-    def generate_states(self, current_day_weather, number):
-
-        future_weather = []
+        self.current_day_weather = next_day_weather
+        print(next_day_weather)
         
-        for i in range(number):
-            next_day = self.__next__(current_day_weather)
-            future_weather.append(next_day)
-            current_day_weather = next_day
-        return future_weather
+        return next_day_weather
+       
 
 
-# One possible implementation is an enumeration class with the different weather types      
-# class WeatherTypes(Enum):
-#     sunny = 0
-#     cloudy = 1
-#     rainy = 2
-#     snowy = 3
-#     windy = 4
-#     hailing = 5
+
--- homework/HW7/HW7-final/P2.py
+++ homework/HW7/HW7-final/P2.py
@@ -15,29 +15,30 @@ cursor.execute("DROP TABLE IF EXISTS model_coefs")
 cursor.execute("DROP TABLE IF EXISTS model_results")
 
 # Create model_params table
+# Create model_params table
 cursor.execute('''CREATE TABLE model_params (
-    id INTEGER PRIMARY KEY NOT NULL,
+    id INTEGER,
     desc TEXT,
     param_name TEXT,
-    value INTEGER)''')
+    value TEXT)''')
 
 db.commit()
 
 # Create model_coefs table
 cursor.execute('''CREATE TABLE model_coefs (
-    id INTEGER PRIMARY KEY NOT NULL,
+    id INTEGER,
     desc TEXT,
     feature_name TEXT,
-    value INTEGER)''')
+    value TEXT)''')
 
 db.commit()
 
 # Create model_results table
 cursor.execute('''CREATE TABLE model_results (
-    id INTEGER PRIMARY KEY NOT NULL,
+    id INTEGER,
     desc TEXT,
-    train_score INTEGER,
-    test_score INTEGER)''')
+    train_score FLOAT,
+    test_score FLOAT)''')
 
 db.commit()
 
@@ -57,27 +58,52 @@ def save_to_database(model_id, model_desc, db, model, X_train, X_test, y_train, 
     # X_train, X_test, y_train, and y_test should be used to compute test_score and train_score
     train_score = model.score(X_train, y_train)
     test_score = model.score(X_test, y_test)
-    param_keys = model.get_params().keys() 
-    param_values = model.get_params().values() # How to convert these values to string
-    param_values = str(model.get_params().values())
     coef, inter = model.coef_, model.intercept_
 
-    vals_to_insert_param = (model_id, model_desc, param_keys, param_values)
-    vals_to_insert_coef = (model_id, model_desc, coef, inter)
-    vals_to_insert_results = (model_id, model_desc, train_score, test_score)
 
-    # Insert the model information to the corresponding tables
-    cursor.execute('''INSERT INTO model_params (id, desc, param_name, value) VALUES (?, ?, ?, ?)''', vals_to_insert_param)
-    cursor.execute('''INSERT INTO model_params (id, desc, feature_name, value) VALUES (?, ?, ?, ?)''', vals_to_insert_coef)
-    cursor.execute('''INSERT INTO model_params (id, desc, train_score, test_score) VALUES (?, ?, ?, ?)''', vals_to_insert_results)
+    # Insert into params table
+    for i, j in model.get_params().items():
+        cursor.execute('''INSERT INTO model_params (id, desc, param_name, value) VALUES (?, ?, ?, ?)''', (model_id, model_desc, i, j))
+    
+    # Insert into coefs table
+    for i, j in zip(data.feature_names, model.coef_[0]):
+        cursor.execute('''INSERT INTO model_coefs (id, desc, feature_name, value) VALUES (?, ?, ?, ?)''', (model_id, model_desc, i, j))
+
+    cursor.execute('''INSERT INTO model_coefs (id, desc, feature_name, value) VALUES (?, ?, ?, ?)''', (model_id, model_desc, "intercept", model.intercept_[0] ))
+    
+    # Insert into results table
+    cursor.execute('''INSERT INTO model_results (id, desc, train_score, test_score) VALUES (?, ?, ?, ?)''', (model_id, model_desc, train_score, test_score))
 
 # Fit model
 baseline_model = LogisticRegression(solver='liblinear')
 baseline_model.fit(X_train, y_train)
 
-# Save to database
+# Save model 1 database
 save_to_database(1, 'Baseline model', db, baseline_model, X_train, X_test, y_train, y_test)
 
+# Reduced logistic regression model
+feature_cols = ['mean radius', 
+                'texture error',
+                'worst radius',
+                'worst compactness',
+                'worst concavity']
+
+X_train_reduced = X_train[feature_cols]
+X_test_reduced = X_test[feature_cols]
+
+reduced_model = LogisticRegression(solver='liblinear')
+reduced_model.fit(X_train_reduced, y_train)
+
+# Save model 2 to database
+save_to_database(2, 'Reduced model', db, reduced_model, X_train_reduced, X_test_reduced, y_train, y_test)
+
+
+# Penalized logistic regression model
+penalized_model = LogisticRegression(solver='liblinear', penalty='l1', random_state=87, max_iter=150)
+penalized_model.fit(X_train, y_train)
+
+# Save model 3 to database
+save_to_database(3, 'L1 penalty model', db, penalized_model, X_train, X_test, y_train, y_test)
 
 
 
